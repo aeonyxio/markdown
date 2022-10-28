36c29ac8915ce569fb80805347ab570f3cd02575
aeonyxio <aeonyxio@gmail.com>
2022-10-28 21:30:25 +0100

    json formatting

--- deps.ts
+++ deps.ts
@@ -9,4 +9,5 @@ export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
 import "https://esm.sh/prismjs@1.27.0/components/prism-typescript?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-bash?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-rust?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-json?no-check";
 import "https://esm.sh/prism-svelte@0.5.0?no-check";
